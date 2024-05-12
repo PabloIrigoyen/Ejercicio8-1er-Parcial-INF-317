56ab766d39f157bbbf33cd4d5ba8cf305d130209
PabloIrigoyen <pvillarroeli@fcpn.edu.bo>
2024-05-12 17:28:48 -0400

    Add files via upload

---
+++ ejer8.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "omp.h"
+
+void generarPares(int *serie, int inicio, int fin, int incremento, int N) {
+    for (int i = inicio; i <= fin; i += incremento) {
+        *serie++ = i;
+    }
+}
+
+int main() {
+    const int M = 4; 
+    const int N = 10; 
+    int *vectores[M];
+    int ini, fin, inc;
+
+    #pragma omp parallel num_threads(M)
+    {
+        int thread_id = omp_get_thread_num();
+        vectores[thread_id] = (int *)malloc(N * sizeof(int));
+
+        ini = (thread_id * 2) + 2;
+        fin = ini + (N - 1) * M;
+        inc = 2 * M;
+        generarPares(vectores[thread_id], ini, fin, inc, N);
+    }
+
+    for (int i = 0; i < M; ++i) {
+        printf("Vector %d: ", i + 1);
+        for (int j = 0; j < N; ++j) {
+            printf("%d ", vectores[i][j]);
+        }
+        printf("\n");
+        free(vectores[i]); 
+    }
+
+    return 0;
+}
